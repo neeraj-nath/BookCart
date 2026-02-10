82a6bf3b9a894e587974c9110a1c3fddd65c3499
neerajnath <neeraj.nath987@gmail.com>
2026-02-10 22:39:05 +0530

    Implicit usings disable and design changes

--- BookCartWeb/Models/ErrorViewModel.cs
+++ BookCartWeb/Models/ErrorViewModel.cs
@@ -1,9 +1,8 @@
-namespace BookCartWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
+namespace BookCartWeb.Models;
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
--- BookCartWeb/Program.cs
+++ BookCartWeb/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
