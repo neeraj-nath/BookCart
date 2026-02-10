9de0c3a1a7ae957541b6802c53765744c2020dbe
neerajnath <neeraj.nath987@gmail.com>
2026-02-10 17:41:53 +0530

    Add project files.

---
+++ BookCartWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookCartWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
